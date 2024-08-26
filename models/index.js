15ba42cfd1cce58675205ee07880295bf5d6682f
Helena Pilon <helenapilon@gmail.com>
2024-08-26 11:04:14 -0300

    Aula 23/08

--- models/index.js
+++ models/index.js
@@ -17,4 +17,17 @@ db.produtos = require("./produto.model.js")(sequelize, Sequelize);
 db.lojas = require("./loja.model.js")(sequelize, Sequelize);
 db.usuarios = require("./usuario.model.js")(sequelize, Sequelize);
 db.categorias = require("./categoria.model.js")(sequelize, Sequelize);
+
+//Relacionamento 1:1 entre Usuario e Loja
+db.usuarios.hasOne(db.lojas);
+db.lojas.belongsTo(db.usuarios);
+
+//Relacionamento 1:* entre Loja e Produto
+db.lojas.hasMany(db.produtos);
+db.produtos.belongsTo(db.lojas);
+
+//Relacionamento *:* entre Produto e Categoria
+db.produtos.belongsToMany(db.categorias, { through: "produtoCategoria" });
+db.categorias.belongsToMany(db.produtos, { through: "produtoCategoria" });
+
 module.exports = db;
