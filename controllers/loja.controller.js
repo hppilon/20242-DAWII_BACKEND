5fa1964c545b36e654b22428b7181040b528ce50
Helena Pilon <helenapilon@gmail.com>
2024-09-06 15:27:39 -0300

    Aula 06/09

--- controllers/loja.controller.js
+++ controllers/loja.controller.js
@@ -32,7 +32,9 @@ exports.findOne = (req, res) => {
       if (data) {
         res.send(data);
       } else {
-        res.status(404).send("Não possível encontrar uma loja com o id" + id);
+        res
+          .status(404)
+          .send({ message: "Não possível encontrar uma loja com o id" + id });
       }
     })
     .catch((err) =>
@@ -44,20 +46,47 @@ exports.findOne = (req, res) => {
 
 exports.update = (req, res) => {
   const _id = req.params.id;
-
   Loja.update(req.body, { where: { id: _id } })
     .then((num) => {
       if (num == 1) {
-        res.send("Loja atualizada com sucesso");
+        res.send({ message: "Loja atualizada com sucesso" });
       } else {
-        res
-          .status(404)
-          .send(
-            "Não foi possível atualizar. Req.body vazio ou loja não encontrada."
-          );
+        res.status(404).send({
+          message:
+            "Não foi possível atualizar. Req.body vazio ou loja não encontrada.",
+        });
       }
     })
     .catch((err) => {
       res.status(500).send({ message: err.message || "Erro ao atualizar" });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Loja.destroy({ where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Loja removida com sucesso" });
+      } else {
+        res.send({ message: "Loja não encontrada. Id: " + id });
+      }
+    })
+    .catch((err) =>
+      res.status(500).send({ message: err.message || "Erro ao deletar loja" })
+    );
+};
+
+exports.deleteAll = (req, res) => {
+  Loja.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((nums) => {
+      res.send({ message: `${nums} lojas foram removidas` });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message || "Erro ao remover lojas" });
     });
 };
--- routes/loja.routes.js
+++ routes/loja.routes.js
@@ -3,6 +3,11 @@ module.exports = (app) => {
   var router = require("express").Router();
 
   router.post("/", lojas.create);
+  router.get("/", lojas.findAll);
+  router.get("/:id", lojas.findOne);
+  router.put("/:id", lojas.update);
+  router.delete("/:id", lojas.delete);
+  router.delete("/", lojas.deleteAll);
 
   app.use("/lojas", router);
 };
